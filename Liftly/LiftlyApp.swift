2e0b04ed5b074826022da17d8404777ce3747a50
Gr33nEight <natanael.jop.app@gmail.com>
2026-04-01 21:27:50 +0200

    first commit

---
+++ Liftly/LiftlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftlyApp.swift
+//  Liftly
+//
+//  Created by Natanael Jop on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct LiftlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
