5f3a8c3611c35e68f18c4a948502df7a57a73251
Gr33nEight <natanael.jop.app@gmail.com>
2026-04-01 22:06:30 +0200

    Custom colors

---
+++ Liftly/DesignSystem/Extensions/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  Liftly
+//
+//  Created by Natanael Jop on 01/04/2026.
+//
+
+import SwiftUI
+
+extension Color {
+    enum custom {
+        static let background = Color("CustomBackground")
+        static let text = Color("CustomText")
+        static let primary = Color("CustomPrimary")
+        static let secondary = Color("CustomSecondary")
+        static let tertiary = Color("CustomTertiary")
+    }
+}
