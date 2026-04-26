30f7e0381e0da490dd906621158c0151eec812dc
KrzysztofS02 <krzysztofs02@o2.pl>
2026-04-26 23:55:18 +0200

    Conected HomeView with MockData

--- Liftly/Presentation/Feature/Home/View/HomeView.swift
+++ Liftly/Presentation/Feature/Home/View/HomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct HomeView: View {
     @StateObject var viewModel: HomeViewModel
     var body: some View {
-        VStack {
+        VStack(spacing: 5){
             HStack {
                 Text("Home")
                     .font(.custom.largeTitle())
@@ -25,98 +25,11 @@ struct HomeView: View {
             }.padding(20)
             
             ScrollView{
-                ForEach (1..<2, id: \.self){ _ in
-                    PostCell()
-                    
-                    PostCell()
-                    
-                    PostCell()
-                    
-                    PostCell()
+                ForEach (MockData.routines, id: \.title){ Rutine in
+                    PostCell(routine: Rutine)
                 }
             }
         }
-    }
-}
-// Posts
-struct PostCell: View {
-    var body: some View {
-        HStack {
-            //profil photo
-            Image(systemName: "person.fill")
-                .resizable()
-                .scaledToFit()
-                .padding(10)
-                .frame(width:50, height: 50)
-                .background(Color.gray.opacity(0.3))
-                .clipShape(Circle())
-            
-            //describe profile
-            VStack(alignment: .leading) {
-                Text("User123")
-                    .font(.headline)
-                
-                Text("3 ours ago")
-                    .font(.subheadline)
-                    .foregroundColor(.custom.tertiary)
-            }
-            
-            Spacer()
-            
-        } .padding()
-        
-        //statistics
-        Text("Describe")
-            .fontWeight(.bold)
-        
-        HStack (spacing: 25){
-            VStack(alignment: .leading){
-                Text("Time")
-                    .font(.caption)
-                    .foregroundColor(.custom.tertiary)
-                Text("1h 30min")
-                    .font(.headline)
-            }
-            
-            VStack(alignment: .leading) {
-                Text("Volume")
-                    .font(.caption)
-                    .foregroundColor(.custom.tertiary)
-                Text("3500kg")
-                    .font(.headline)
-            }
-            
-            VStack(alignment:.leading) {
-                Text("Records")
-                    .font(.caption)
-                    .foregroundColor(.custom.tertiary)
-                
-                HStack(spacing: 4) {
-                    Image(systemName: "medal.fill")
-                        .foregroundColor(.yellow)
-                    Text("4")
-                }
-            }
-            
-            VStack{
-                Text("Acg Bpm")
-                    .font(.caption)
-                    .foregroundColor(.custom.tertiary)
-                HStack(spacing: 4) {
-                    Image(systemName: "heart.fill")
-                        .foregroundColor(.red)
-                    Text("117")
-                }
-            }
-        }
-        
-        VStack (spacing: 10){
-            SimpleExcerciseRow(iconName: "figure.strengthtraining.traditional", exerciseText: "4 sets Bench Press")
-            
-            SimpleExcerciseRow(iconName: "figure.walk", exerciseText: "4 stets Lunge")
-            
-            SimpleExcerciseRow(iconName: "figure.strengthtraining.traditional", exerciseText: "4 Gorllla Row")
-        }.padding()
     }
 }
 
