a09db03b29221d4f1d3a4c48fc2d16e8fa009ff7
KrzysztofS02 <krzysztofs02@o2.pl>
2026-06-05 12:13:33 +0200

    added Validation in RegisterView

--- Liftly/Presentation/Feature/Authentication/Register/RegisterView.swift
+++ Liftly/Presentation/Feature/Authentication/Register/RegisterView.swift
@@ -7,9 +7,25 @@
 
 import SwiftUI
 
+enum ValidationResult {
+    case success
+    case failure(String)
+    
+    var errorMessage: String? {
+        switch self {
+        case .success: return nil
+        case .failure(let msg): return msg
+        }
+    }
+}
+
 struct RegisterView: View {
+    @State private var fName: String = ""
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    @State private var showError: Bool = false
+
     var body: some View {
         ZStack{
             Color.custom.background
@@ -17,15 +33,54 @@ struct RegisterView: View {
             
             VStack{
                 Text("Sign in")
+                    .font(.title)
+                    .padding(.bottom, 40)
+                    .padding(.top, 16)
                 
-                CustomTextField(text: $email, placeholder: "Enter your email")
+                Divider()
+                    .background(Color.custom.tertiary)
                 
-                CustomTextField(text: $password, placeholder: "Passwor", isPassword: true)
+                CustomTextField(text: $fName, iconName: "person", placeholder: "Name")
+                    .padding(.vertical, 30)
+                if showError, let error = validateFname().errorMessage {
+                    Text(error)
+                        .foregroundColor(Color.red)
+                        .padding(.top, -35)
+                }
+            
+                CustomTextField(text: $email, iconName: "envelope", placeholder: "Email")
+                    .padding(.bottom, 30)
+                if showError, let error = validateEmail().errorMessage {
+                    Text(error)
+                        .foregroundColor(Color.red)
+                        .padding(.top, -35)
+                }
                 
-                CustomTextField(text: $password, placeholder: "Enter your password again", isPassword: true)
+                CustomTextField(text: $password, iconName: "lock", placeholder: "Password", isPassword: true)
+                    .padding(.bottom, 30)
+                if showError, let error = validatePassword().errorMessage {
+                    Text(error)
+                        .foregroundColor(Color.red)
+                        .padding(.top, -35)
+                }
                 
-                Button(action: {}) {
+                CustomTextField(text: $confirmPassword, iconName: "lock", placeholder: "Password again", isPassword: true)
+                    .padding(.bottom, 30)
+                if showError, let error = validateConfirmPasswor().errorMessage {
+                    Text(error)
+                        .foregroundColor(Color.red)
+                        .padding(.top, -35)
+                }
+                
+                Button(action: {
+                    if isFormValid{
+                        print("Register success")
+                    } else {
+                        showError = true
+                    }
+                }) {
                     Text("Sign in")
+                        .frame(maxWidth: .infinity)
                 }
                 .padding()
                 .background(Color.custom.secondary)
@@ -35,6 +90,51 @@ struct RegisterView: View {
         }
         .foregroundColor(Color.custom.text)
     }
+    
+    var isFormValid: Bool {
+        [
+            validateFname(),
+            validateEmail(),
+            validatePassword(),
+            validateConfirmPasswor()
+        ].allSatisfy {
+            if case .success = $0 { return true }
+            else{
+                return false
+            }
+        }
+    }
+    
+    func validateFname() -> ValidationResult {
+        fName.isEmpty ? .failure("Name is required") : .success
+    }
+    
+    func validateEmail() -> ValidationResult {
+        let emailPattern = #"^\S+@\S+\.\S+$"#
+        if email.range(of: emailPattern, options: .regularExpression) == nil {
+            return.failure("Enter a valid email")
+        }
+        return .success
+    }
+        
+    func validatePassword() -> ValidationResult {
+        if password.count < 8 {
+            return .failure("Passwor must be atleast 8 chatacters")
+        }
+        return .success
+    }
+    
+    func validateConfirmPasswor() -> ValidationResult {
+        if confirmPassword.isEmpty {
+            return .failure("Please confirm password")
+        }
+        
+        if confirmPassword != password {
+            return .failure("Passwors not match")
+        }
+        return .success
+    }
+    
 }
 
 #Preview {
