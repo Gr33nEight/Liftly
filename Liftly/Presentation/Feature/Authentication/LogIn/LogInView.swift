2f3e05f28581cb085b8f46366559f67cf851ecea
KrzysztofS02 <krzysztofs02@o2.pl>
2026-06-06 19:41:50 +0200

    Validation of Login and Password

--- Liftly/Presentation/Feature/Authentication/LogIn/LogInView.swift
+++ Liftly/Presentation/Feature/Authentication/LogIn/LogInView.swift
@@ -7,9 +7,24 @@
 
 import SwiftUI
 
+enum ValidationResult {
+    case success
+    case failure(String)
+    
+    var errorMessage: String? {
+        switch self {
+        case .success: return nil
+        case .failure(let msg): return msg
+        }
+    }
+}
+
 struct LogInView: View {
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var correctEmai: String = "Test@gmail.com"
+    @State private var correctPassword: String = "Test1234"
+    @State private var showError: Bool = false
     var body: some View {
         ZStack{
             Color.custom.background
@@ -33,11 +48,27 @@ struct LogInView: View {
                 
                 CustomTextField(text: $email, iconName: "envelope", placeholder: "Email")
                     .padding(.vertical, 30)
+                if showError, let error = validateEmail().errorMessage {
+                    Text(error)
+                        .foregroundStyle(Color.red)
+                        .padding(.top, -35)
+                }
                 
                 CustomTextField(text: $password, iconName: "lock", placeholder: "Password", isPassword: true)
                     .padding(.bottom, 30)
+                if showError, let error = validPassword().errorMessage {
+                    Text(error)
+                        .foregroundStyle(Color.red)
+                        .padding(.top, -35)
+                }
                 
-                Button(action: {}) {
+                Button(action: {
+                    if isFormValid{
+                        print("Login success")
+                    } else {
+                        showError = true
+                    }
+                }) {
                     Text("Login")
                         .frame(maxWidth: .infinity)
                 }
@@ -57,6 +88,33 @@ struct LogInView: View {
             
         }
         .foregroundColor(Color.custom.text)
+    }
+    
+    var isFormValid: Bool {
+        [
+            validateEmail(),
+            validPassword(),
+        ].allSatisfy {
+            if case .success = $0 { return true }
+            else{
+                return false
+            }
+        }
+    }
+    
+    func validateEmail() -> ValidationResult {
+        let emailPattern = #"^\S+@\S+\.\S+$"#
+        if email.range(of: emailPattern, options: .regularExpression) == nil && email != correctEmai {
+            return.failure("Enter a correct Email addres")
+        }
+        return .success
+    }
+    
+    func validPassword() -> ValidationResult {
+        if password.count < 8 && password != correctPassword {
+            return .failure("Enter correct Password")
+        }
+        return .success
     }
 }
 
